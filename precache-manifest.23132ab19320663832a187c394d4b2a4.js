c3d41a6fbab0b8795e04bf68fc8f0768cc84a5e0
Nick Huemmer <njhuemmer@gmail.com>
2021-10-04 12:10:26 -0400

    Updates

---
+++ precache-manifest.23132ab19320663832a187c394d4b2a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57b6205d63b232faacc5",
+    "url": "/course-directory/static/css/main.fbc75975.chunk.css"
+  },
+  {
+    "revision": "57b6205d63b232faacc5",
+    "url": "/course-directory/static/js/main.57b6205d.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "81552d18bcd3ac25b7e4515808a23a62",
+    "url": "/course-directory/index.html"
+  }
+];
